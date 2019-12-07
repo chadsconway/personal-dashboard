39388054da28eb1999a9744c42b727181ba44364
Chad Conway <agile4java@gmail.com>
2019-12-06 18:12:35 -0600

    removed .env from cached files

--- public/js/loadtheme.js
+++ public/js/loadtheme.js
@@ -19,4 +19,23 @@ function getTheme() {
 			alert('Theme update failed with error: ' + error);
 		}
 	});
+
+	/**
+	 *  Server doesn't like ajax request, going with an xmlhttprequest example
+	 *  from W3Schools
+	 */
+
+	// var xhttp = new XMLHttpRequest();
+	// xhttp.onreadystatechange = function() {
+	// 	if (this.readyState == 4 && this.status == 200) {
+	// 		// Typical action to be performed when the document is ready:
+	// 		// document.getElementById('demo').innerHTML = xhttp.responseText;
+	// 		console.log(xhttp.responseText);
+	// 		console.log(xhttp.status);
+	// 		console.log('XML response: ', xhttp.responseXML);
+	// 		jQuery('#themelink').attr('href', 'public/css/bootstrap.css');
+	// 	}
+	// };
+	// xhttp.open('GET', 'http://localhost:3500/api/gettheme', true);
+	// xhttp.send();
 }
